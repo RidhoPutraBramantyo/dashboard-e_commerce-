4beb78727acdb6ee71cf742b1a335be6b918bcf7
RidhoPutraBramantyo <126537811+RidhoPutraBramantyo@users.noreply.github.com>
2023-12-09 11:00:27 +0800

    "perbaikan atas push sebelunya"
    - kesalahan pada "... <Provider store={store}>..." salah menambah "...store={store}..." padahal belum diperlukan
    - kesalahan menambah Redux yang sebelumnya sudaha ada malah ditambah lagi. Akibatnya double Redux dengan model yang berbeda satu sama lain
    - kesalahan menaruh redux yang harusnya di project "Pizza Pizzaz" malah di project "ecommerce-dashboard"

--- src/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-// src/redux/reducers/index.js
-import { combineReducers } from "redux";
-import exampleReducer from "./exampleReducer";
-
-export default combineReducers({
-  example: exampleReducer,
-});
